3605d6d37304c989c10068161912fb068697b289
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2013-08-02 09:58:57 +0200

    Start adding the user interface

--- src/Main.hs
+++ src/Main.hs
@@ -2,18 +2,20 @@ module Main where
 
 import Filter
 import Hash
-{-import System.Environment (getArgs)-}
-{-import qualified Data.ByteString.Lazy.Char8 as BSC-}
+import System.Environment (getArgs)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.ByteString.Lazy.Char8 as BSCL
 import Data.BloomFilter
 import Data.BloomFilter.Easy
 import Data.BloomFilter.Hash
+import System.Exit
 
 main :: IO ()
-main = undefined
-
+main = do
+    args <- getArgs
+    case args of
+        _ ->  putStrLn help >> exitFailure
 
 createFilter :: Int -- ^ Expected size
              -> Double -- ^ False positive rate
@@ -24,4 +26,11 @@ createFilter n f filterDataFile = do
     let (numBits, numHashes) = suggestSizing n f
     let dataLines = map (Hash.hash64 . BSCL.toStrict) $ BSCL.lines filterData
     return $ fromListB (cheapHashes numHashes) numBits dataLines
+
+
+help :: String
+help = "webqual-filter: removes graph nodes that correspond to web pages\n" ++
+       "that gave an error response code.\n\n" ++ 
+       "USAGE:\n" ++
+       "    webqual-filter filterFile numElements"
 
